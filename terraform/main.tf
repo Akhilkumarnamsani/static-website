33b5dd38c24634df0964cfcdb93449fe4a1bcc8f
Akhilkumarnamsani <Akhilkumar.namsani@gmail.com>
2025-05-07 20:12:25 +0100

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -15,6 +15,31 @@ resource "aws_s3_bucket" "static_site" {
   }
 }
 
+resource "aws_s3_bucket_public_access_block" "static_site" {
+  bucket = aws_s3_bucket.static_site.id
+
+  block_public_acls       = false
+  block_public_policy     = false
+  ignore_public_acls      = false
+  restrict_public_buckets = false
+}
+
+resource "aws_s3_bucket_policy" "public_read" {
+  bucket = aws_s3_bucket.static_site.id
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Principal = "*"
+        Action = "s3:GetObject"
+        Resource = "${aws_s3_bucket.static_site.arn}/*"
+      }
+    ]
+  })
+}
+
 resource "aws_s3_bucket_object" "index" {
   bucket = aws_s3_bucket.static_site.bucket
   key    = "index.html"
