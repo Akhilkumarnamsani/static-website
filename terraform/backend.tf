fb72b78bc07c5533b13ca4b200c8fe4c96a08173
Akhilkumarnamsani <Akhilkumar.namsani@gmail.com>
2025-05-07 19:36:35 +0100

    Update backend.tf

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket         = "my-static-s3-bucket"
+    bucket         = "akil-s3-static-site-test-bucket"
     key            = "terraform.tfstate"
     region         = "eu-west-2"
     use_lockfile   = true
