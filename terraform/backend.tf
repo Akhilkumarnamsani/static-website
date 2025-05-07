ccabddab5c27e48ccde9f3788a40c4343efb4ad7
Akhilkumarnamsani <Akhilkumar.namsani@gmail.com>
2025-05-07 19:54:29 +0100

    Update backend.tf

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -3,7 +3,6 @@ terraform {
     bucket         = "akil-s3-static-site-test-bucket"
     key            = "terraform.tfstate"
     region         = "eu-west-2"
-    use_lockfile   = true
     encrypt        = true
   }
 }
